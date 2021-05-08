526649628fdeaf168d437c710b4f0ab6542dc9f9
Vartika-Srivastava <vartikavarti27@gmail.com>
2021-05-08 14:16:07 +0530

    new file

--- src/components/Attractions.js
+++ src/components/Attractions.js
@@ -1,9 +1,11 @@
 import React from 'react'
 
-export default function Attractions() {
+function Attractions() {
     return (
         <div>
             
         </div>
     )
 }
+
+export default Attractions
